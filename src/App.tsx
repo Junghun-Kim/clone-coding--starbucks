96a1d8c7e44adfdbc4dd7fbd4401e5e583944d47
junghun_kim <vienna453@gmail.com>
2025-01-28 22:30:13 +0900

    [feat] Routing 일부 구현
    - package 설치
      - react-router-dom
      - styled-components
    - title 변경
    - App Component에서 RouterProvider render
    - Layout.tsx, Home.tsx, Profile.tsx 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout.tsx';
+import Home from './routes/Home.tsx';
+import Profile from './routes/Profile.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      }
+    ],
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <RouterProvider router={router} />
+    </>
   );
 }
 
