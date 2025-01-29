7a35d2b64b83eb5c05273dcee1710a627aabf1a7
junghun_kim <vienna453@gmail.com>
2025-01-29 23:02:17 +0900

    [feat] Login, CreateAccount component 구현
    - Login, CreateAccount component 구현
    - globalStyles render

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import './App.css';
 import Layout from './components/Layout.tsx';
+import CreateAccount from './routes/CreateAccount.tsx';
 import Home from './routes/Home.tsx';
+import Login from './routes/Login.tsx';
 import Profile from './routes/Profile.tsx';
 
 const router = createBrowserRouter([
@@ -19,13 +23,34 @@ const router = createBrowserRouter([
         element: <Profile />,
       }
     ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount />,
   }
 ]);
+
+const GlobalStyles = createGlobalStyle`
+${reset};
+* {
+  box-sizing: border-box;
+}
+body {
+  background-color: black;
+  color:white;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`;
 
 function App() {
   return (
     <>
-    <RouterProvider router={router} />
+      <GlobalStyles />
+      <RouterProvider router={router} />
     </>
   );
 }
