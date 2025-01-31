0ac5a25a1f88f88ead2838960a3ecb264c22b4f4
junghun_kim <vienna453@gmail.com>
2025-01-31 16:02:53 +0900

    [set] eslint, prettier 세팅
    - eslint, prettier 패키지 설치

--- src/routes/CreateAccount.tsx
+++ src/routes/CreateAccount.tsx
@@ -1,19 +1,21 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div``
 
-const Form = styled.form``;
+const Form = styled.form``
 
-const Input = styled.input``;
+const Input = styled.input``
 
 export default function CreateAccount() {
-    return <Wrapper>
-        <Form>
-            <Input name='name' placeholder="Name" type='text' />
-            <Input />
-            <Input />
-            <Input />
-        </Form>
-    </Wrapper>;
+  return (
+    <Wrapper>
+      <Form>
+        <Input name="name" placeholder="Name" type="text" />
+        <Input />
+        <Input />
+        <Input />
+      </Form>
+    </Wrapper>
+  )
 }
